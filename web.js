8f0a4cadc6e8fe0a2d241214872b8a0eb67b1f6d
Srinidhi.Koushik <koushiksrinidhi@gmail.com>
2013-07-09 03:49:19 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,12 +1,4 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  response.send("Hello World from index.html" );
 });
